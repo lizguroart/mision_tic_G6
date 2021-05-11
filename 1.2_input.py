9832b73c40c287e79ec60f723b25efaf22deea02
lizguroart <dianyguti@gmail.com>
2021-05-11 18:36:10 -0500

    Codigo sesion 5

---
+++ 1.2_input.py
@@ -0,0 +1,8 @@
+print('Manejo de información de entrada con python')
+print('-------------------------------------------')
+primer_nombre = input("Por favor ingrese su nombre: ") #Guardo la información ingresada por teclado en la variable primer_nombre
+primer_apellido = input("Por favor ingrese su apellido: ") #Guardo la información ingresada por teclado en la variable primer_apellido
+print("Hola" ,primer_nombre, primer_apellido, "es un gusto saludarte")
+comida_favorita = input("¿Cual es tu comida favorita? ")
+color_favorito = input("y Cual es tu color favorito? ")
+print("Me gusta mucho el", comida_favorita, "pero el color", color_favorito, "no es mi color favorito" )
---
+++ 1.3_tipo_datos_int.py
@@ -0,0 +1,5 @@
+primer_nombre = input("Por favor ingrese su nombre: ")
+edad = int(input("Ingrese edad: "))
+
+print(type(primer_nombre))
+print(type(edad))
